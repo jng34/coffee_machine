78079cb3bbc938372cb94684ed21ec1e158a5b1f
James Ng <njames15@gmail.com>
2022-12-15 11:50:42 -0500

    refactored code

--- main.py
+++ main.py
@@ -1,51 +1,6 @@
-from os import system, name
 from art import logo
 from info import MENU, resources
 
-################################
-def clear():
-    """Function to clear console"""
-    # for windows
-    if name == 'nt':
-        _ = system('cls')
-    # for mac and linux
-    else:
-        _ = system('clear')
-################################
-
-
-
-# MENU = {
-#     "espresso": {
-#         "ingredients": {
-#             "water": 50,
-#             "coffee": 18,
-#         },
-#         "cost": 1.5,
-#     },
-#     "latte": {
-#         "ingredients": {
-#             "water": 200,
-#             "milk": 150,
-#             "coffee": 24,
-#         },
-#         "cost": 2.5,
-#     },
-#     "cappuccino": {
-#         "ingredients": {
-#             "water": 250,
-#             "milk": 100,
-#             "coffee": 24,
-#         },
-#         "cost": 3.0,
-#     }
-# }
-
-# resources = {
-#     "water": 1000,
-#     "milk": 800,
-#     "coffee": 500,
-# }
 
 w, m, c = resources
 money_earned = 0
